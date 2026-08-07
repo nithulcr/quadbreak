d2cf314700c26963811692d92a81258b07291185
nithul <nithulcr@gmail.com>
2026-08-07 22:53:29 +0530

    wesawe

--- src/components/Hero-old.tsx
+++ src/components/Hero-old.tsx
@@ -19,6 +19,7 @@ export default function Hero() {
 
       const ctx = gsap.context(() => {
         const heroBanner = section.querySelector(".hero-banner");
+        const video = section.querySelector(".video-shrink video");
 
         // Scroll-linked scale animation for hero banner
         if (heroBanner) {
@@ -39,7 +40,24 @@ export default function Hero() {
           );
         }
 
-
+        // Scroll-linked scale animation for video
+        if (window.innerWidth > 990 && video) {
+          gsap.fromTo(
+            video,
+            { scale: 0.1 },
+            {
+              scale: 1,
+              ease: "none",
+              scrollTrigger: {
+                trigger: heroBanner,
+                start: "top top",
+                end: "bottom top",
+                scrub: true,
+                invalidateOnRefresh: true,
+              },
+            }
+          );
+        }
 
         // Individual fade-up animations for each element
         const fadeUpElements = gsap.utils.toArray<Element>(section.querySelectorAll(".fade-up"));
@@ -87,9 +105,10 @@ export default function Hero() {
   }, []);
 
   return (
-    <section ref={sectionRef} className="relative flex flex-col justify-center overflow-hidden main-hero px-5 lg:px-[4rem] pb-6 pt-15 lg:py-20 min-h-screen">
-
-        <div className="fade-up-css  hero-banner  flex items-center flex-col justify-center max-w-[1360px] mx-auto pt-20 px-5 w-full gap-5 ">
+    <section ref={sectionRef} className="relative flex flex-col overflow-hidden main-hero px-5 lg:px-[4rem] pb-6 pt-15 lg:py-20 ">
+      <img src="/images/bg.png" alt="Shape 1" className="absolute top-0 left-0 w-full opacity-50 z-[-1]" />
+      <div className="relative hero-banner w-full flex flex-col items-start justify-between bg-cover bg-no-repeat bg-center hero-bg-img">
+        <div className="fade-up-css flex items-center flex-col justify-center max-w-[1360px] mx-auto pt-20 px-5 w-full gap-5 min-h-[calc(100vh-260px)]">
           <h1 className="text-white uppercase  max-w-5xl text-center text-[3rem] lg:text-[6rem]  leading-none mb-1">
             We&rsquo;re smart
             and crazy
@@ -98,31 +117,31 @@ export default function Hero() {
           <p className="text-white text-[16px] md:text-[18px] max-w-2xl text-center mx-auto lg:mx-0 font-light">
             Evolved from Wrinit, Quadbreak Studios extends your team with expertly crafted 3D assets and environments — built for games, virtual reality, and simulation projects. Creative, reliable, and production-ready
           </p>
-          {/* <div className="md:flex items-center justify-center max-w-[1360px] mx-auto  w-full gap-5 fade-up-css">
-            <AnimatedButton
-              href=""
-              label="Explore Our Work"
-              className="w-fit"
-            />
-            <AnimatedButton
-              href="s"
-              label="Get in Touch"
-              className="w-fit"
-            />
-          </div> */}
           <span className="rounded-full w-7 h-7 border border-gray-300 flex items-center justify-center">
             <ArrowDown className="w-4 h-4 text-gray-300" />
           </span>
         </div>
-
-
-      <div className="absolute top-0 left-0 w-full  z-[-1]">
+        
+        <div className="md:flex items-center hidden justify-between max-w-[1360px] mx-auto pt-10 lg:pt-15 w-full gap-5 fade-up-css">
+          <AnimatedButton
+            href=""
+            label="Explore Our Work"
+            className="w-fit"
+          />
+          <AnimatedButton
+            href="s"
+            label="Get in Touch"
+            className="w-fit"
+          />
+        </div>
+      </div>
+      <div className="video-shrink mt-[-75px] flex justify-start items-start" style={{ width: "100%", height: "100%" }}>
         <video
           autoPlay
           loop
           muted
           playsInline
-          className=""
+          className="rounded-14px] lg:rounded-[40px] origin-top"
         >
           <source src="/images/video.mp4" type="video/mp4" />
           Your browser does not support the video tag.
