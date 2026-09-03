b67514850a52c5e93a9642d6cbf94779324f79a9
nithul <nithulcr@gmail.com>
2026-09-03 17:34:15 +0530

    weaaaaaaaaaaaaaaaa

--- src/components/CTASection.tsx
+++ src/components/CTASection.tsx
@@ -64,7 +64,7 @@ const CTASection = () => {
   return (
     <section
       ref={sectionRef}
-      className="relative overflow-hidden  bg-white/3"
+      className="relative overflow-hidden  bg-white/3 my-10"
     >
       <div className="mx-auto max-w-[1450px] px-5">
         <div
